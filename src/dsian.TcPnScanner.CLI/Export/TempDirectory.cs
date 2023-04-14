c1c5530cd69ab6cec7c750413e7273f22f5af560
densogiaichned <dsian@posteo.de>
2023-04-14 12:34:35 +0200

    fix(TempDirectory): Replace template $APPNAME$

--- src/dsian.TcPnScanner.CLI/Export/TempDirectory.cs
+++ src/dsian.TcPnScanner.CLI/Export/TempDirectory.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace dsian.TcPnScanner.CLI.Export
 {
     internal class TempDirectory
@@ -12,6 +5,7 @@ namespace dsian.TcPnScanner.CLI.Export
         internal static FileInfo CreateFileInfo(string exportFilePath, string controller = Constants.DEFAULT_CONTROLLER_NAME)
         {
             exportFilePath = exportFilePath.Replace(Constants.TEMP_DIR_TEMPLATE, Path.GetTempPath());
+            exportFilePath = exportFilePath.Replace(Constants.APPNAME_TEMPLATE, AssemblyHelper.Name);
             exportFilePath = exportFilePath.Replace(Constants.CONTROLLER_FILENAME_TEMPLATE, controller);
             var exportDir = Path.GetDirectoryName(exportFilePath);
             if (!Directory.Exists(exportDir) && !string.IsNullOrEmpty(exportDir))
